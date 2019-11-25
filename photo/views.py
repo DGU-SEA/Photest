a787f0d18a2756d5c417e26b0d35959d7f6dcb3d
hyex <hyexkim@gmail.com>
2019-11-26 00:59:29 +0900

    blog(instagram) 8-9

--- photo/views.py
+++ photo/views.py
@@ -7,6 +7,11 @@ from django.views.generic.detail import DetailView
 from .models import Photo
 
 
+from django.http import HttpResponseRedirect
+
+from django.contrib import messages
+
+
 class PhotoList(ListView):
     model = Photo
     template_name_suffix = '_list'
@@ -34,12 +39,30 @@ class PhotoUpdate(UpdateView):
     model = Photo
     fields = ['author','text', 'image']
     template_name_suffix = '_update'
-    success_url = '/'
+    # success_url = '/'
+
+    def dispatch(self, request, *args, **kwargs):
+        object = self.get_object()
+        if object.author != request.user:
+            messages.warning(request, '수정할 권한이 없습니다.')
+            return HttpResponseRedirect('/')
+        else:
+            return super(PhotoUpdate, self).dispatch(request, *args, **kwargs)
+
 
 class PhotoDelete(DeleteView):
     model = Photo
     template_name_suffix = '_delete'
     success_url = '/'
+
+    def dispatch(self, request, *args, **kwargs):
+        object = self.get_object()
+        if object.author != request.user:
+            messages.warning(request, '삭제할 권한이 없습니다.')
+            return HttpResponseRedirect('/')
+        else:
+            return super(PhotoDelete, self).dispatch(request, *args, **kwargs)
+
 
 
 class PhotoDetail(DetailView):
