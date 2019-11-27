a13b3326e16bf5bafbb6398be65a7081d441a576
hyex <hyexkim@gmail.com>
2019-11-27 20:50:04 +0900

    delete venv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/kimhyeji/Desktop/SoftwareEngineering/Photest/venv/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
