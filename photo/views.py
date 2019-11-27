8fd5935bc5b0cf7316d83e0eb8c88b81396b9624
yujinHan97 <dbwlslek97@gmail.com>
2019-11-27 09:17:59 +0900

    url connection

--- photo/views.py
+++ photo/views.py
@@ -1,39 +1,52 @@
-from django.shortcuts import render
-
-from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.views.generic.detail import DetailView
+from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect
+from django.shortcuts import render
+from django.utils import timezone
 from .models import Photo
-
 
 from django.http import HttpResponseRedirect
 
 from django.contrib import messages
 
+def main(request):
+    return render(request, 'photo/main.html', {})
+
+def join(request):
+    return render(request, 'photo/join.html', {}) 
+    
+def board(request):
+    #best = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')*/
+    return render(request, 'photo/board.html', {}) #, {'posts': posts}
+
+def best(request):
+    #best = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')*/
+    return render(request, 'photo/best.html', {}) #, {'posts': posts}
+
+
+def hashtag_board(request):
+    #best = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')*/
+    return render(request, 'photo/hashtag_board.html', {}) #, {'posts': posts}
 
 class PhotoList(ListView):
     model = Photo
-    template_name_suffix = '_list'
-
+    template_name_suffix='_list'
 
 class PhotoCreate(CreateView):
     model = Photo
-    fields = ['author','text', 'image']
-    template_name_suffix = '_create'
-    success_url = '/'
-    
+    fields=['text', 'image']
+    template_name_suffix='_create'
+    success_url='/'
+
     def form_valid(self, form):
-        form.instance.author_id = self.request.user.id
+        form.instance.author_id=self.request.user.author_id
         if form.is_valid():
-            # 올바르다면
-            # form : 모델폼
             form.instance.save()
             return redirect('/')
         else:
-            # 올바르지 않다면
-            return self.render_to_response({'form': form}) 
-
+            return self.render_to_response({'form':form})
 
 class PhotoUpdate(UpdateView):
     model = Photo
@@ -49,7 +62,6 @@ class PhotoUpdate(UpdateView):
         else:
             return super(PhotoUpdate, self).dispatch(request, *args, **kwargs)
 
-
 class PhotoDelete(DeleteView):
     model = Photo
     template_name_suffix = '_delete'
@@ -63,9 +75,6 @@ class PhotoDelete(DeleteView):
         else:
             return super(PhotoDelete, self).dispatch(request, *args, **kwargs)
 
-
-
 class PhotoDetail(DetailView):
     model = Photo
-    template_name_suffix = '_detail'
-
+    template_name_suffix='_detail'
