08c55b0b5b90b3329c46bc17913f772c552d9b99
hyex <hyexkim@gmail.com>
2019-11-30 04:20:36 +0900

    join

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    reportCnt = models.IntegerField(default=0)
+    likeCnt = models.IntegerField(default=0)
