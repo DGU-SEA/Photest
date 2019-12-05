d482d0741ade21a6b89d3e8f3e4fee1d6e9655b1
hyex <hyexkim@gmail.com>
2019-12-05 16:54:49 +0900

    refactoring except photo app

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from django.db.models.signals import post_save
-# from django.dispatch import receiver
 
+# Django에서 제공하는 User 모델을 그대로 사용하며, 추가적인 필드를 덧붙이기 위해 Profile을 사용
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     reportCnt = models.IntegerField(default=0)
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.http.response import HttpResponseRedirect
-# from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib import auth
 from .models import * 
@@ -8,32 +7,37 @@ from django.contrib.auth.hashers import check_password
 from photo.models import Photo
 from hashtag.models import hashtag
 
+# join, logout, mypage, report, reward 함수
+
 def join(request):
     if request.method == "POST":
-
+        # html 으로부터 넘어온 모든 값
         username = request.POST["username"]
-        password1=request.POST["password1"]
-        password2=request.POST["password2"]
-        email=request.POST["email"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+        email = request.POST["email"]
 
+        # 중복 ID 체크
         if User.objects.filter(username=username).exists() :
             return render(request, 'accounts/join.html', { 'is_used' : "중복된 ID 입니다."})
 
+        # 모든 필드 미작성 시
         if (username == "") or (password1 == "") or (password2 == "") or (email == ""):
-            return render(request, 'accounts/join.html', { 'is_used' : "모든 필드를 입력해주세요."})
+            return render(request, 'accounts/join.html', {'is_used':"모든 필드를 입력해주세요."})
 
+        # 패스워드 불일치
         if request.POST["password1"] == request.POST["password2"]:
             user = User.objects.create_user(
                 username=request.POST["username"],
                 password=request.POST["password1"],
-            	email=request.POST["email"])
+            	email=request.POST["email"] )
             profile = Profile(user=user)
             profile.save()
             auth.login(request,user)
-            # return HttpResponseRedirect(reverse('photo/main'))
             return redirect('/')  
         return render(request, 'accounts/join.html', {'is_used' : "패스워드가 일치하지 않습니다."})
     return render(request, 'accounts/join.html', {})
+
 
 def logout(request):
     auth.logout(request)  
@@ -42,6 +46,7 @@ def logout(request):
 def mypage(request):
     global Photo
 
+    # 로그인 시에만 mypage 접근 가능
     if not request.user.is_authenticated:
         print("user is not authenticated")
         return redirect('/')
@@ -53,7 +58,8 @@ def mypage(request):
         if request.user == p.author :
             # print(p.image.url)
             photos.append(p)
-            
+
+    # 정보 수정 시
     if request.method == "POST":
         password=request.POST["password"]
         email=request.POST["email"]
@@ -67,10 +73,10 @@ def mypage(request):
             request.user.save()
             return render(request, 'accounts/mypage.html', {'photos' : photos})
         return render(request, 'accounts/mypage.html', {'photos' : photos})
-        
-    #best = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')*/
+
     return render(request, 'accounts/mypage.html', {'photos' : photos})
 
+# 신고받은 사진의 작성자 신고받은 횟수 +1
 def report(request) :
     if request.method == "GET":
         author = request.GET['author']
@@ -79,24 +85,21 @@ def report(request) :
         user.profile.save()
         return HttpResponseRedirect(request.GET['path'])
 
-
+# 관리자가 날짜를 선택하면 해당 날짜 1-5등의 reward +1
 def reward(request) :
     if request.method == "GET":
         date = request.GET['user_date']
-        print("date" + date)
-        type(date)
         tag = ""
 
         for h in hashtag.objects.all():
             if (str(h.tagDate) == date):
                 tag = h.tagName
                 print("tag : " + tag)
-                # tags.append(h.tagName)
-                # print(h.tagName)
 
         photos = Photo.objects.all().order_by('-like')
         bestPhotos =[]
         index = 0
+
         for p in photos :
             for h in p.hashtag['tag']:
                 # print(p.hashtag['tag'])
@@ -114,4 +117,3 @@ def reward(request) :
             user.profile.save()
 
         return render(request, 'accounts/mypage.html', {'bestPhotos': bestPhotos})
-
